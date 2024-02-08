f2bd394464c5e5fb1dba6f9ba24b70985141f529
Anubis1Good <vlad.polyakov.96@inbox.ru>
2024-02-08 12:02:33 +0300

    version for teacher1

--- win.js
+++ win.js
@@ -15,12 +15,7 @@ function startScript(someFunc){
 document.querySelector('.main__BO_btn').onclick = () => {
     startScript(openLessons)
 }
-document.querySelector('.main__checkbox_btn').onclick = () => {
-    startScript(forgiveCheckbox)
-}
-document.querySelector('.main__openQuiz_btn').onclick = () => {
-    startScript(openQuiz)
-}
+
 
 
 function openLessons(){
@@ -34,45 +29,7 @@ function openLessons(){
     }
 }
 
-function forgiveCheckbox(){
-    const check_btn = document.querySelector('.dHsxLw')
-    const restart_btn = document.querySelector('.jMZWwi')
-    if (check_btn && restart_btn){
-        check_btn.classList.remove('dHsxLw')
-        check_btn.classList.add('gchTFx')
-        restart_btn.classList.remove('jMZWwi')
-        restart_btn.classList.add('boGcTb')
 
-    }
-    const tasks = document.querySelector('.iNogWr')
-    if (tasks){
-        tasks.classList.remove('iNogWr')
-        tasks.classList.add('gBtcRu')
-
-    }
-
-}
-function openQuiz(){
-    const blockTask = document.querySelector('.ggYfwG')
-    const blockOverlay = document.querySelector('.kdPPyv')
-    if (blockTask && blockOverlay){
-        blockTask.style.display = 'flex'
-        blockOverlay.style.display = 'none'
-        const btn_next = document.createElement('button')
-        blockTask.appendChild(btn_next)
-        btn_next.innerHTML = 'next➡'
-        btn_next.onclick = ()=>{
-            const adress = window.location.href
-            const reg = /level=\w*&/
-            const start = adress.search(reg)+6
-            const end = adress.indexOf('&',start)
-            const task = adress.slice(start,end)
-            const newAdress = adress.slice(0,start) + (parseInt(task)+1) + adress.slice(end,adress.length)
-            window.location.href = newAdress
-            
-        }
-    }
-}
 
 
 
